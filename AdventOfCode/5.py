b26cc6dbe8a731afc4cda9bf45083c3f1d655f17
Dylan Meeus <meeusdylan@hotmail.com>
2017-12-05 21:29:35 +0100

    day 5 part 1

---
+++ AdventOfCode/5.py
@@ -0,0 +1,27 @@
+# Problem 5 of advent of code
+
+
+# find out how many steps it take to escape a list
+# "0 3 0 1 -3"
+
+
+def getInput():
+    f = open("input5.txt",'r')
+    inp = f.read()
+    strNumbers = inp.split("\n")
+    return list(map(lambda k: int(k), strNumbers[:-1]))
+
+def solve():
+    print("solving problem")
+    input = getInput()
+    i = 0
+    steps = 0
+    while(i < len(input)):
+        jmp = input[i]
+        input[i] = jmp + 1
+        i += jmp
+        steps += 1
+
+    print(steps)
+
+solve()
