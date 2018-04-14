05b462420c8fb75e2758570d8ade7595bebc48d2
Dylan Meeus <meeusdylan@hotmail.com>
2018-04-14 23:49:15 +0200

    bst

--- raytracer/raytracer.py
+++ raytracer/raytracer.py
@@ -1,6 +1,76 @@
 # Raytracer implementation
+import math
+
+class Triple:
+    """ Triple class and some operations that make sense on them """
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def add(self, other):
+        self.a += other.a
+        self.b += other.b
+        self.c += other.c
+
+    def subtract(self, other):
+        self.a -= other.a
+        self.b -= other.b
+        self.c -= other.c
+
+    def __eq__(self, other):
+        return  self.a == other.a and self.b == other.b and self.c == other.c
+
+    def __str__(self):
+        return "{" + str(self.a) + ", " + str(self.b) + ", " + str(self.c) + "}"
+
+class Vector3(Triple):
+    def __init__(self, a, b, c):
+        Triple.__init__(self,a,b,c)
+
+    def scalar_multi(self, scalar):
+        scalar = float(scalar)
+        self.a *= scalar
+        self.b *= scalar
+        self.c *= scalar
+
+    def scalar_div(self, scalar):
+        scalar = float(scalar)
+        self.a /= scalar
+        self.b /= scalar
+        self.c /= scalar
+
+    def vector_multi(self, vector):
+        self.a *= vector.a
+        self.b *= vector.b
+        self.c *= vector.c
+
+    def cross(self, vector):
+        new_a = (self.b * vector.c) - (self.c * vector.b)
+        new_b = (-(self.a * vector.c) - (self.c * vector.a))
+        new_c = (self.a * vector.b) - (self.b * vector.a)
+        self.a = new_a
+        self.b = new_b
+        self.c = new_c
+
+    def dot(self, vector):
+        return float(self.a * vector.a + self.b * vector.b + self.c * vector.c)
+
+    def length(self):
+        return math.sqrt(a*a + b*b + c*c)
+    
+    def get_unit_vector(self):
+        """ return the current vectors unit vector representation """
+        k = 1.0 / self.length()
+        return Vector3(self.a * k, self.b * k, self.c *k)
+
 
 if __name__ == '__main__':
+    a = Vector3(1,2,3)
+    b = Vector3(3,2,1)
+
+def test():
+#if __name__ == '__main__':
     xs = 200
     ys = 100
     max_rgb = 255
---
+++ trees/bst.py
@@ -0,0 +1,57 @@
+
+
+class node:
+    def __init__(self, value = None, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class tree:
+    def __init__(self, root = None):
+        self.root = root
+
+    def append(self, new_node):
+        if self.root == None:
+            self.root = new_node
+        else:
+            self._append(self.root, new_node)
+        
+    # recursive function to find where the node needs to be
+    def _append(self, node, new_node):
+        if node == None:
+            return new_node
+        if  new_node.value < node.value:
+            node.left = self._append(node.left, new_node)
+        elif new_node.value > node.value:
+            node.right = self._append(node.right, new_node)
+        return node
+
+
+
+    def pre_order_print(self):
+        self.pre_order_print(self.root)
+
+    # recursive function to print the tree
+    def _pre_order_print(self, node):
+        print(node.value)
+
+        if node.left != None:
+            self.pre_order_print(node.left)
+
+        if node.right != None:
+            self.pre_order_print(node.right)
+           
+
+        
+
+if __name__ == '__main__':
+    t = tree()
+    t.append(node(10))
+    t.append(node(5))
+    t.append(node(20))
+    t.append(node(1))
+    t.append(node(6))
+    t.append(node(15))
+    t.pre_order_print()
+
